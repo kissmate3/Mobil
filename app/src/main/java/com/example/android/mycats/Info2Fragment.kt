635dff72da7e3c8ae90f62f164aea961ab14aece
kissmate3 <kissmate222@gmail.com>
2020-12-04 15:28:23 +0100

    fix info3

---
+++ app/src/main/java/com/example/android/mycats/Info2Fragment.kt
@@ -0,0 +1,26 @@
+package com.example.android.mycats
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.example.android.mycats.databinding.FragmentInfo2Binding
+import com.example.android.mycats.databinding.FragmentInfoBinding
+
+class Info2Fragment : Fragment() {
+    private lateinit var binding: FragmentInfo2Binding
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        binding = DataBindingUtil.inflate<FragmentInfo2Binding>(inflater, R.layout.fragment_info2, container, false)
+        binding.Next2button.setOnClickListener { view: View ->
+            view.findNavController().navigate(Info2FragmentDirections.info2Toinfo3())
+        }
+        binding.Backbutton.setOnClickListener { view: View ->
+            view.findNavController().navigate(Info2FragmentDirections.inf2Toinf())
+        }
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/example/android/mycats/Info3Fragment.kt
@@ -0,0 +1,28 @@
+package com.example.android.mycats
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+
+import com.example.android.mycats.databinding.FragmentInfo3Binding
+
+class Info3Fragment : Fragment() {
+    private lateinit var binding: FragmentInfo3Binding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate<FragmentInfo3Binding>(inflater, R.layout.fragment_info3, container, false)
+        binding.FinishButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(Info3FragmentDirections.info3Totitlefrag())
+        }
+        binding.Back2Button.setOnClickListener { view: View ->
+            view.findNavController().navigate(Info3FragmentDirections.inf3toinf2())
+        }
+        return binding.root
+    }
+}
