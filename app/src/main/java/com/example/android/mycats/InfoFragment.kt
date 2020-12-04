4098bf3e999e659947569131b9df8c74f2b2b3b5
kissmate3 <kissmate222@gmail.com>
2020-12-04 10:50:17 +0100

    updgrade info

--- app/src/main/java/com/example/android/mycats/InfoFragment.kt
+++ app/src/main/java/com/example/android/mycats/InfoFragment.kt
@@ -5,11 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.example.android.mycats.databinding.FragmentInfoBinding
+
+
 
 class InfoFragment : Fragment() {
+    private lateinit var binding: FragmentInfoBinding
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_info, container, false)
+         binding = DataBindingUtil.inflate<FragmentInfoBinding>(inflater, R.layout.fragment_info, container, false)
+         binding.NextButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(InfoFragmentDirections.infoToinfo2())
+        }
+        return binding.root
     }
 }
